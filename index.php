b25509acada1d53c16fa2e7c1f2afd20adf97546
Faysweb92 <faissal.lazaar@lepoles.org>
2023-07-27 10:53:05 +0200

    projet R_F

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div>
+<h1>Formulaire</h1>
+
+<form action="/action_page.php">
+  <label for="fname">Firstname:</label><br>
+  <input type="text" id="firsname" name="fname" value=""><br>
+  <label for="lname">Lastname:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <input type="submit" value="Submit"><br><br>
+  <label for="email">Enter your email:</label><br>
+  <input type="email" id="email" name="email"><br><br>
+  <input type="submit"value="Submit">
+</form> 
+</div>
+
+    
+</body>
+</html>
